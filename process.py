203629bee5352fcc1902af51934cfb7b832f978d
黎金宁 <lijinning@sjtu.edu.cn>
2017-10-16 17:42:19 +0800

    initialize

---
+++ process.py
@@ -0,0 +1,69 @@
+import codecs
+import os
+import sys
+import datetime
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+rootdir = os.getcwd()
+
+def hexoCalling():
+    print ("\n\n--------------- Calling Hexo Contributor ---------------\n\n")
+    os.chdir(rootdir)
+    os.system('hexo clean')
+    os.system('hexo generate')
+
+def insertRealIndex():
+    print ("\n\n--------------- Insert Homepage ---------------\n\n")
+    publicdir = rootdir + '/' + 'public'
+    os.system('mv ' + publicdir + '/' + 'index.html ' + publicdir + '/' + 'blog.html')
+    for root, dirs, files in os.walk(rootdir + '/' + 'HomePage'):
+        for file in files:
+            if file == '.DS_Store':
+                continue
+            print ('Extracting ' + root + '/' + file)
+            os.system('cp ' + root + '/' + file + ' ' + publicdir + '/' + file)
+        for dir in dirs:
+            if dir == '.git':
+                continue
+            print ('Extracting Directory ' + root + '/' + dir)
+            os.system('cp -r ' + root + '/' + dir + ' ' + publicdir + '/' + dir)
+        break
+
+
+def MoveToGithubDir():
+    print ("\n\n--------------- Moving To Github Directory ---------------\n\n")
+    githubdir = rootdir + '/' + 'jinningli.github.io'
+
+    for root, dirs, files in os.walk(rootdir + '/' + 'public'):
+        for file in files:
+            if file == '.DS_Store':
+                continue
+            print ('Extracting ' + root + '/' + file)
+            os.system('cp ' + root + '/' + file + ' ' + githubdir + '/' + file)
+        for dir in dirs:
+            if dir == '.git':
+                continue
+            print ('Extracting Directory ' + root + '/' + dir)
+            os.system('cp -r ' + root + '/' + dir + ' ' + githubdir + '/' + dir)
+        break
+
+
+def pushToGithub():
+    print ("\n\n--------------- Pushing To Github Directory ---------------\n\n")
+    githubdir = rootdir + '/' + 'jinningli.github.io'
+    os.chdir(githubdir)
+    print('git add .')
+    os.system('git add .')
+    print('git commit -m \"Updated, AutoCommit at ' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\"')
+    os.system('git commit -m \"Updated, AutoCommit at ' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\"')
+    print('git push')
+    os.system('git push')
+
+
+
+hexoCalling()
+insertRealIndex()
+MoveToGithubDir()
+# pushToGithub()
+
---
+++ source/_posts/hello World.md
@@ -0,0 +1,16 @@
+---
+title: Hello World!
+tags:
+  - Hello World
+categories: Notes
+date: 2017-10-16 15:25:00
+updated: 2017-10-16 15:25:00
+thumbnail:
+---
+# Hello World!
+- Hello
+- world
+
+```
+int main()
+```
